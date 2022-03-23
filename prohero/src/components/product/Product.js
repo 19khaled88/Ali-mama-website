f11f61ebb8ec705dab54d8f7e585f1b603439457
19khaled88 <kkhaled88hasan@gmail.com>
2022-03-23 16:56:23 +0600

    cart 1st phase code

--- prohero/src/components/product/Product.js
+++ prohero/src/components/product/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './product.css'
 export const Product = (props) => {
+    const{addToCart} = props
     const {name,seller,price,ratings,img} = props.product
   return (
     <div className="card-view">
@@ -10,8 +11,9 @@ export const Product = (props) => {
             <p>Price: ${price}</p>
             <p><small>Manufacture:{seller}</small></p>
             <p><small>Ratings:{ratings}</small></p>
+            <p>{addToCart}</p>
         </div>
-        <button>Add to Cart</button>
+        <button onClick={()=>addToCart(props.product)}>Add to Cart</button>
     </div>
   )
 }
--- prohero/src/components/shop/Shop.js
+++ prohero/src/components/shop/Shop.js
@@ -3,17 +3,22 @@ import { Product } from '../product/Product';
 import './shop.css'
 export const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     
     useEffect(()=>{
         fetch('products.json')
             .then(res => res.json())
             .then(data =>setProducts(data))
-    },[])
+    },[]);
+    const addToCart=(product)=>{
+        const newCart =[...cart, product];
+        setCart(newCart);
+        console.log(cart);
+    }
   return (
-
     <div className="shop-div">
         <div className="product-container">
-         {products.map(product=><Product key={product.id} product={product}></Product>)}   
+         {products.map(product=><Product key={product.id} product={product} addToCart={addToCart}></Product>)}   
         </div>
         <div className="order-summary">
             <h3>Order summary</h3>
