e9df4595dc7ee911cadd33f88434466bb3f37f80
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-03-23 22:16:39 +0600

    commit addToCart Codes

--- prohero/src/components/product/Product.js
+++ prohero/src/components/product/Product.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import './product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 export const Product = (props) => {
-    const {name,seller,price,ratings,img} = props.product
+  const { name, seller, price, ratings, img } = props.product
+  const { addToCart } = props
   return (
     <div className="card-view">
-        <img src={img} alt="" />
-        <div className="card-info">
-            <p>{name}</p>
-            <p>Price: ${price}</p>
-            <p><small>Manufacture:{seller}</small></p>
-            <p><small>Ratings:{ratings}</small></p>
-        </div>
-        <button>Add to Cart</button>
+      <img src={img} alt="" />
+      <div className="card-info">
+        <p>{name}</p>
+        <p>Price: ${price}</p>
+        <p>
+          <small>Manufacture:{seller}</small>
+        </p>
+        <p>
+          <small>Ratings:{ratings}</small>
+        </p>
+       
+      </div>
+      <button onClick={()=>addToCart(props.product)}>
+        <span>Add to Cart</span>
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   )
 }
--- prohero/src/components/shop/Shop.js
+++ prohero/src/components/shop/Shop.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Product } from '../product/Product';
+import { Product } from '../product/Product'
 import './shop.css'
 export const Shop = () => {
-    const [products, setProducts] = useState([]);
-    
-    useEffect(()=>{
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data =>setProducts(data))
-    },[])
+  const [products, setProducts] = useState([])
+  const addToCart = (product) => {
+    console.log(product.id)
+  }
+  useEffect(() => {
+    fetch('products.json')
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+  }, [])
   return (
-
     <div className="shop-div">
-        <div className="product-container">
-         {products.map(product=><Product key={product.id} product={product}></Product>)}   
-        </div>
-        <div className="order-summary">
-            <h3>Order summary</h3>
-        </div>
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="order-summary">
+        <h3>Order summary</h3>
+      </div>
     </div>
   )
 }
